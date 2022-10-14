099855563e6a73c7b823c4e5076c481b6e16c9f7
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-13 23:51:26 -0400

    Fix the links, I'm so used to markdown I wrote markdown in HTML xD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,10 @@
                 </div>
                 
                 <div class="font-mono mt-4 mb-2">
-                    If you're curious and would like to contribute, [this whole project](https://github.com/lazy-build/service) is open source [including the scripts](https://github.com/Lazy-Build/scripts). Contributions are always welcome, and multiple versions are not needed, but also welcome!
+                    If you're curious and would like to contribute, 
+                    <a href="https://github.com/lazy-build/service">this whole</a> project is open source
+                    <a href="https://github.com/Lazy-Build/scripts">including the scripts</a>. 
+                    Contributions are always welcome, and multiple versions are not needed, but also welcome!
                 </div>
                 <!-- This example requires Tailwind CSS v2.0+ -->
                 <ul role="list" class="w-full flex flex-wrap gap-4 dark:text-gray-200">
